cd77d93752c9b74efd9fe3837c5855baf2dfead5
lucy berger <lucyblueglass@gmail.com>
2016-06-20 19:17:45 -0700

    satisfy first spec

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+//jQuery business logic
+
+var leapYear = function(year) {
+  return false;
+};
+
+
+//jQuery user interface logic
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
